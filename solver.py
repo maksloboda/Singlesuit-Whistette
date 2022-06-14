f9535e35720cc8f2e412ef8a21934ca089dac875
Max <maksloboda123@gmail.com>
2022-06-14 18:02:17 +0300

    Added stateless solver, Added stress test

---
+++ solver.py
@@ -0,0 +1,73 @@
+from typing import List, Tuple, Any
+from basic_driver import EMPTY_FIELD, BasicDriver
+import itertools
+
+class GameState:
+  first_player_set: List[int]
+  second_player_set: List[int]
+  current_player: int
+  field: int
+
+  def __init__(self,
+      first_player_set_: List[int],
+      second_player_set_: List[int],
+      current_player_ : int,
+      field_: int):
+    self.first_player_set = first_player_set_
+    self.second_player_set = second_player_set_
+    self.current_player = current_player_
+    self.field = field_
+
+def prepare_legacy_solver(state: GameState) -> Tuple[Any, List[int]]:
+  """
+  Returns a solver that is in the provided state and a sorted list of cards
+  """
+  other_player = 1 - state.current_player
+  extra_card = [(state.field, other_player)] if state.field is not EMPTY_FIELD else []
+  # An array of (card_number, owner)
+  card_info = list(
+      sorted(
+          itertools.chain(
+              zip(state.first_player_set, [0] * len(state.first_player_set)),
+              zip(state.second_player_set, [1] * len(state.second_player_set)),
+              extra_card
+          )
+      )
+  )
+
+  card_owners = list(map(lambda x: x[1], card_info))
+  # An array of (card_number, owner) but cards are renamed
+  new_card_info = list(
+    map(
+      lambda x: (x[0] + 1, x[1]),
+      enumerate(card_owners)
+    )
+  )
+
+  solver = BasicDriver(
+    list(
+      map(
+        lambda x: x[0],
+        filter(
+          lambda x: x[1] == other_player,
+          new_card_info
+        )
+      )
+    ),
+    extra_card != []
+  )
+
+  if extra_card:
+    # make the fake move
+    card_index = card_info.index(extra_card[0])
+    assert(card_index != -1)
+    solver.move_by_player(card_index + 1)
+
+  card_names = list(map(lambda x: x[0], card_info))
+
+  return (solver, card_names)
+
+def find_optimal_move(state: GameState):
+  solver, card_names = prepare_legacy_solver(state)
+  idx = solver.move_by_computer()
+  return card_names[idx - 1]
--- tests.py
+++ tests.py
@@ -1,6 +1,8 @@
+import random
 import unittest
 from basic_driver import BasicDriver
 from test_util.random_state_picker import generate_random_state
+from solver import find_optimal_move, GameState
 
 repeats = 10000
 
@@ -26,12 +28,22 @@ class TestSolverStress(unittest.TestCase):
       bd2 = BasicDriver(second_cards, first_player == 1)
       while not bd1.is_end() and not bd2.is_end():
         self.assertNotEqual(bd1.can_move_by_player(), bd2.can_move_by_player())
+        computer, user = bd1, bd2
+        first_player = 1
         if bd1.can_move_by_player():
-          card = bd2.move_by_computer()
-          bd1.move_by_player(card)
-        else:
-          card = bd1.move_by_computer()
-          bd2.move_by_player(card)
+          computer, user = bd2, bd1
+          first_player = 0
+        predicted_card = find_optimal_move(
+          GameState(
+            bd1.user_cards,
+            bd2.user_cards,
+            first_player,
+            computer.field,
+          )
+        )
+        card = computer.move_by_computer()
+        self.assertEqual(card, predicted_card)
+        user.move_by_player(card)
       self.assertEqual(bd1.is_end(), bd2.is_end())
 
 
